c2fd3f941a7276311422d13e7e23da8a378435c1
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-08-07 21:31:46 +0000

    counter new styles

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
-import '../../styles/bootstrap.min.css'; // Ajusta la ruta según tu estructura
+import '../../styles/bootstrap.min.css'; 
+import './SecondsCounter.css'; 
 
 class SecondsCounter extends Component {
   render() {
     const { seconds } = this.props;
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
     return (
-      <div className="counter card text-center p-4">
-        <div className="card-body">
-          <h1 className="card-title"><FontAwesomeIcon icon={faClock} /> {seconds} segundos</h1>
+      <div className="counter">
+        <div className="time-box">
+          <div className="page">{hours}</div>
+          <div className="page">{minutes}</div>
+          <div className="page">{remainingSeconds}</div>
+        </div>
+        <div className="card text-center paper-effect">
+          <div className="card-body">
+            <h1 className="card-title"><FontAwesomeIcon icon={faClock} /> Tiempo transcurrido</h1>
+          </div>
         </div>
       </div>
     );
